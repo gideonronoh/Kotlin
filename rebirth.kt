f644a5eb93009fffb6206666f97b28f3c564269d
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-10-18 16:14:56 +0300

    re.kt

---
+++ rebirth.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var cartTotal = 0
+    cartTotal = 20
+    println("Total: $cartTotal")
+}
+fun main() {
+    var count = 10
+    println("You have $count unread messages.")
+    count = count + 1
+}
+fun main() {
+    var count = 10
+    println("You have $count unread messages.")
+    count++
+    println("You have $count unread messages.")
+}
