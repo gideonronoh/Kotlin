66b18f684f2ab50e1016baf23cd3b150ec2461fc
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-10-19 12:12:40 +0300

    $ n variables.kt

---
+++ $ n variables.kt
@@ -0,0 +1,16 @@
+fun main(){
+   birthdayGreeting() 
+} 
+fun birthdayGreeting(){
+    val name = "Rover"
+    val age = "15"
+    val output =" $name,happy birthday, you are now $age!"
+    print (output)
+}
+fun main (){
+    val meeting = "DevFest"
+    val date = "November 4th"
+    val purpose = "enrich GDSC"
+    val reminder =  "$meeting will happen on $date so as to $purpose"
+    print(reminder)
+}
