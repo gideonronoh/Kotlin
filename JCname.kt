0eba02de33b20ed4f00ccaef19cb923117fb3059
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-11-16 19:58:57 +0300

    JCname.kt

---
+++ JCname.kt
@@ -0,0 +1,8 @@
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hi, my name is $name!")
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!")
+}
