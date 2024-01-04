9196cd66649d4168bc195f8079cbce4d24ce6914
Gideon Rono <145061874+28ron@users.noreply.github.com>
2024-01-04 22:11:16 +0300

    constructor.kt

---
+++ constructor.kt
@@ -0,0 +1,15 @@
+class person()
+var fullName; String
+init{
+fullName = "$firstNaME $lastName"
+}fun introduve()
+{
+println
+("Hello,my name is $fullName.")
+}
+}
+fun main()
+{
+val person = Person("John","Doe")
+person.introduce()
+}
